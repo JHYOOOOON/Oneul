34044d1959a2ca0c330e48e81c1621844ff9492a
jhyoon <jhyooooooon@gmail.com>
2022-12-05 21:19:22 +0900

    style: reset css 적용

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export default createGlobalStyle`
+	${reset}
 	html {
 		font-family: "Pretendard", sans-serif;
 	}
