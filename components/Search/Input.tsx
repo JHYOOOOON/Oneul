642e0cf429cbd203cf38db09517e89f48bc28d70
jhyoon <jhyooooooon@gmail.com>
2023-01-27 23:34:09 +0900

    feat: input 값 전체 삭제 버튼 추가 #13

--- components/Search/Input.tsx
+++ components/Search/Input.tsx
@@ -1,6 +1,7 @@
-import styled from "styled-components";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import { TiDelete } from "react-icons/ti";
 import { useResetRecoilState } from "recoil";
+import styled from "styled-components";
 
 import { useSearch } from "@/components/Search/hooks";
 import { withPage } from "@/state";
@@ -9,6 +10,7 @@ const Input = () => {
 	const [value, setValue] = useState("");
 	const { setSearchValue, getSearchDatas } = useSearch();
 	const resetPage = useResetRecoilState(withPage);
+	const isValueExist = useMemo(() => value.length > 0, [value]);
 
 	const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
 		setValue(event.target.value);
@@ -25,30 +27,62 @@ const Input = () => {
 		getSearchDatas(value);
 	};
 
+	const handleDeleteValue = () => setValue("");
+
 	return (
-		<form onSubmit={handleSubmit}>
+		<StyledForm onSubmit={handleSubmit}>
 			<StyledInput placeholder="어떤 곡을 자주 들으시나요?" onChange={handleChange} value={value} />
-		</form>
+			{isValueExist && (
+				<StyledDeleteButton type="button" onClick={handleDeleteValue}>
+					<TiDelete />
+				</StyledDeleteButton>
+			)}
+		</StyledForm>
 	);
 };
 
 export default Input;
 
-const StyledInput = styled.input`
-	width: 400px;
+const StyledForm = styled.form`
+	position: relative;
+	display: flex;
+	width: 350px;
 	max-width: 80%;
-	height: 25px;
+	height: 32px;
 	margin-bottom: 15px;
 	border-radius: 3px 3px 0 0;
-	border: none;
 	border-bottom: 2px solid ${({ theme }) => theme.color.primary300};
+	background-color: ${({ theme }) => theme.color.gray400};
+	overflow: hidden;
+`;
+
+const StyledInput = styled.input`
+	flex: 1;
+	border-radius: 3px 3px 0 0;
+	border: none;
 	padding: 5px 7px;
 	font-size: ${({ theme }) => theme.textSize.base}rem;
-	background-color: ${({ theme }) => theme.color.gray400};
+	background-color: transparent;
 	transition: border 0.2s;
-
 	&:focus {
 		outline: none;
-		border-bottom: 2px solid ${({ theme }) => theme.color.primary100};
+	}
+`;
+
+const StyledDeleteButton = styled.button`
+	display: flex;
+	align-items: center;
+	height: 100%;
+	position: absolute;
+	top: 0;
+	right: 0;
+	border: none;
+	transition: all 0.2s;
+	font-size: ${({ theme }) => theme.textSize.xxl}rem;
+	color: ${({ theme }) => theme.color.gray};
+	cursor: pointer;
+	&:hover {
+		color: ${({ theme }) => theme.color.black100};
+		transform: rotate(90deg);
 	}
 `;
