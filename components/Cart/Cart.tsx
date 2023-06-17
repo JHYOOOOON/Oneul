0fbf00a946547dfff65e0fe74b2c679365e97541
jhyoon <jhyooooooon@gmail.com>
2023-06-18 02:45:20 +0900

    fix: React 선언하지 않음

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import styled, { css } from "styled-components";
 import { useRecoilValue } from "recoil";
 import { RxHamburgerMenu } from "react-icons/rx";
