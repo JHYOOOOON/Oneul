ed2f3e63310854b1fc50d93cd49d98d9c86036c4
JHYOOOOON <jhyooooooon@gmail.com>
2023-09-01 17:01:49 +0900

    fix: 뒤로가기 버튼 로직 변경

--- components/Recommendation/BackButton.tsx
+++ components/Recommendation/BackButton.tsx
@@ -2,13 +2,12 @@ import { useRouter } from "next/router";
 import { IoChevronBackOutline } from "react-icons/io5";
 import styled from "styled-components";
 
-import { ROUTES } from "@/constants";
 import { SimpleButton } from "@/styles";
 
 export function BackButton() {
 	const router = useRouter();
 	return (
-		<StyledButton onClick={() => router.push(ROUTES.SEARCH)} title="뒤로가기">
+		<StyledButton onClick={() => router.back()} title="뒤로가기">
 			<IoChevronBackOutline />
 			<p>뒤로가기</p>
 		</StyledButton>
