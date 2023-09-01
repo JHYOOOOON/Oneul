195c0c6679240b1deeb0302ce0915f09ef36571e
JHYOOOOON <jhyooooooon@gmail.com>
2023-09-01 16:55:56 +0900

    feat: main 페이지
    추천 페이지, 최근 많이 들은 곡 페이지 들어가기 전 메인 페이지

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,4 +2,5 @@ export const ROUTES = {
 	HOME: "/",
 	SEARCH: "/search",
 	RECOMMENDATION: "/recommendation",
+	RECENT: "/recent",
 };
---
+++ pages/main.tsx
@@ -0,0 +1,49 @@
+import { Suspense } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { Loader, Tooltip } from "@/components";
+import { LogoutButton } from "@/components";
+import { ROUTES } from "@/constants";
+import { Button, PageWrapper } from "@/styles";
+import styled from "styled-components";
+
+export default function Search() {
+	const router = useRouter();
+
+	return (
+		<>
+			<Head>
+				<title>Main | Oneul</title>
+			</Head>
+			<Suspense fallback={<Loader position="center" size="full" />}>
+				<PageWrapper>
+					<LogoutButton />
+					<Wrapper>
+						<Tooltip contents={<p>평소 즐겨듣는 곡을 담은 후, 이를 바탕으로 추천곡을 받아볼 수 있습니다.</p>}>
+							<StyledButton onClick={() => router.push(ROUTES.SEARCH)}>추천곡 찾기</StyledButton>
+						</Tooltip>
+						<Tooltip contents={<p>최근 몇 개월 내에 많이 들은 곡을 확인할 수 있습니다.</p>}>
+							<StyledButton onClick={() => router.push(ROUTES.RECENT)}>많이 들은 곡</StyledButton>
+						</Tooltip>
+					</Wrapper>
+				</PageWrapper>
+			</Suspense>
+		</>
+	);
+}
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: ${({ theme }) => theme.textSize.base}rem;
+	margin-top: 10vh;
+`;
+
+const StyledButton = styled(Button)`
+	width: 70%;
+	max-width: 500px;
+	min-width: 250px;
+	padding: 20px 25px;
+	font-size: ${({ theme }) => theme.textSize.xl}rem;
+`;
